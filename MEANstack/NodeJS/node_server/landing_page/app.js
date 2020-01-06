0ef47f90d00411ff6b5a177b8aaf33537e20d17e
Zach Kraft <tbkrft@gmail.com>
2020-01-06 17:53:06 -0600

    update file

---
+++ MEANstack/NodeJS/node_server/landing_page/app.js
@@ -0,0 +1,33 @@
+const http = require('http');
+const fs = require('fs');
+const server = http.createServer(function(request, response){
+   console.log('client request URL: ', request.url);
+   if(request.url === '/'){
+      fs.readFile('index.html', 'utf8', function(errors, contents){
+         response.writeHead(200, {'Content-Type': 'text/html'})
+         response.write(contents);
+         response.end();
+      })
+   }
+   else if(request.url === '/ninjas'){
+      fs.readFile('ninjas.html', 'utf8', function(errors, contents){
+         response.writeHead(200, {'Content-Type': 'text/html'})
+         response.write(contents);
+         response.end();
+      })
+   }
+   else if(request.url === '/dojos/new'){
+      fs.readFile('dojos.html', 'utf8', function(errors, contents){
+         response.writeHead(200, {'Content-Type': 'text/html'})
+         response.write(contents);
+         response.end();
+      })
+   }
+   else{
+      response.writeHead(404);
+      response.end('File not Found!!!')
+   }
+})
+
+server.listen(6789)
+console.log("Running in localhost at prot 6789")
