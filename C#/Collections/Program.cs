18a2669ac7c2b1dea6bb5cecf4c80f137ee95bd7
tbkrft567 <tbkrft567@gmail.com>
2020-02-13 14:46:00 -0600

    collections completed

--- C#/Collections/Program.cs
+++ C#/Collections/Program.cs
@@ -1,12 +1,70 @@
-﻿using System;
+﻿    using System;
+    using System.Collections.Generic; //this is to include Lists or Dictionaries
 
-namespace Collections
-{
+    namespace Collections
+    {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // ! Array -- fixed in size not internal values
+            // //defaulted to all 0s
+            // int[] numArray = new int[5];
+
+            // //size determined by the given data
+            // int[] newArray2 = {1,2,3,4,5};
+
+            // int[] array3; //Declare
+            // array3 = new int[] {1,3,5,7,9}; //Initialize
+
+            // //Array size cannot change, but values inside can change
+            // newArray2[0] = 9;
+            // Console.WriteLine(newArray2[0]);
+            // foreach (var value in newArray2)
+            // {
+            //     Console.WriteLine(value);
+            // }
+
+            // ! Lists -- similiar to JS array
+            //     List<string> bikes = new List<string>();
+            //     bikes.Add("Kawaski");
+            //     bikes.Add("Triumph");
+            //     Console.WriteLine(bikes[1]);
+            //     Console.WriteLine($"We currently know of {bikes.Count} motorcycle manfacturers");
+
+            //     System.Console.WriteLine("The current manufacturers we have seen are:");
+            //     for (int indx = 0; indx < bikes.Count; indx++)
+            //     {   
+            //         System.Console.WriteLine("-" + bikes[indx]);
+            //     }
+            //     bikes.Insert(2, "Yamaha");
+
+            //     bikes.Remove("Kawaski");
+            //     bikes.RemoveAt(0);
+            //     System.Console.WriteLine("List of Non-Japanese Manufacturers:");
+            //     foreach (var manu in bikes)
+            //     {
+            //         System.Console.WriteLine("-" + manu);
+            //     }
+
+            // ! Dictionary
+            Dictionary<string, string> profile = new Dictionary<string, string>();
+
+            profile.Add("Name", "Speros");
+            profile.Add("Language", "PHP");
+            profile.Add("Location", "Greece");
+            System.Console.WriteLine("Instructor Profile:");
+            System.Console.WriteLine("Name - " + profile["Name"]);
+            System.Console.WriteLine("From - " + profile["Location"]);
+            System.Console.WriteLine("Favorite Language - " + profile["Language"]);
+            // foreach (var item in profile)
+            // {
+            //     System.Console.WriteLine(item.Key + "-" + item.Value);
+            // }
+            foreach (KeyValuePair<string, string> item in profile)
+            {
+                System.Console.WriteLine(item.Key + "-" + item.Value);
+            }
         }
     }
 }
---
+++ C#/CollectionsPractice/Program.cs
@@ -0,0 +1,48 @@
+﻿    using System;
+    using System.Collections.Generic;
+
+    namespace CollectionsPractice
+    {
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ! Arrays
+            // int[] numArray = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            // int[] numArray2 = new int[10];
+
+            string[] strArray = { "Tim", "Martin", "Nikki", "Sara" };
+            // string[] strArray2 = new string[4];
+
+            // bool[] boolArray = { true, false, true, false, true, false, true, false, true, false };
+            // bool[] boolArray2 = new bool[10];
+
+            // ! List
+            List<string> iceCream = new List<string>();
+            iceCream.Add("type1");
+            iceCream.Add("type2");
+            iceCream.Add("type3");
+            iceCream.Add("type4");
+            iceCream.Add("type5");
+            // Console.WriteLine("Ice Creams Available: " + iceCream.Count);
+            // Console.WriteLine("Ice Creams number 3: " + iceCream[2]);
+            iceCream.RemoveAt(2);
+            // Console.WriteLine("Ice Creams Available: " + iceCream.Count);
+
+            // ! Dictionary
+            Random rand = new Random();
+            Dictionary<string, string> stringerDict = new Dictionary<string, string>();
+            for (int index = 0; index < strArray.Length; index++)
+            {
+                stringerDict.Add(strArray[index], iceCream[rand.Next(0, 4)]);
+            }
+            Console.WriteLine($"Peoples Favorite Ice Cream");
+
+            foreach (KeyValuePair<string, string> element in stringerDict)
+            {
+                Console.WriteLine($"{element.Key}-{element.Value}");
+            }
+
+        }
+    }
+}
