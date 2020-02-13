5d8162742623962cb0d1653310fdd27deda94883
tbkrft567 <tbkrft567@gmail.com>
2020-02-13 13:48:15 -0600

    fundamentals completed

---
+++ C#/Funds1/Program.cs
@@ -0,0 +1,75 @@
+﻿    using System;
+
+    namespace Funds1
+    {
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1-255
+            // for (int i = 1; i < 256; i++)
+            // {
+            //     Console.WriteLine(i);
+            // }
+            
+            int ii = 1;
+            while (ii < 256)
+            {
+                System.Console.WriteLine(ii);
+                ii++;
+            }
+
+            //1-100
+            // for (int g = 1; g < 101; g++)
+            // {
+            //     if ((g % 3 == 0 || g % 5 == 0) && (g % 3 != 0 || g % 5 != 0))
+            //     {
+            //     Console.WriteLine(g);
+            //     }
+            // }
+
+            int gg = 1;
+            while (gg < 101)
+            {
+                if ((gg % 3 == 0 || gg % 5 == 0) && (gg % 3 != 0 || gg % 5 != 0))
+                {
+                Console.WriteLine(gg);
+                }
+                gg++;
+            }
+
+            //Fizz -- Buzz
+            // for (int z = 1; z < 101; z++)
+            // {
+            //     if ((z % 3 == 0 || z % 5 == 0) && (z % 3 != 0 || z % 5 != 0))
+            //     {
+            //     if (z % 3 == 0)
+            //     {
+            //         Console.WriteLine("Fizz");
+            //     }
+            //     else
+            //     {
+            //         Console.WriteLine("Buzz");
+            //     }
+            //     }
+            // }
+
+            int zz = 1;
+            while(zz < 101)
+            {
+                if ((zz % 3 == 0 || zz % 5 == 0) && (zz % 3 != 0 || zz % 5 != 0))
+                {
+                    if (zz % 3 == 0)
+                    {
+                        Console.WriteLine("Fizz");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Buzz");
+                    }
+                }
+                zz++;
+            } 
+        }
+    }
+    }
---
+++ C#/PracticeCode/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace PracticeCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            string place = "Coding Dojo";
+            string message = $"Hello {place}";
+            Console.WriteLine(message);
+
+            Console.WriteLine("the" + 3.14);
+            Console.WriteLine("My name is {0}, I am " + 28 + " years old ", "tim");
+
+            // A==B vs. A.Equal(B)
+            //A.Equals(B) checks spot in memory?
+
+            for(int x = 0; x < 5; x++)
+            {
+                Console.WriteLine("X1: "+x);
+            }
+            int y = 0;
+            while(y < 5)
+            {
+                Console.WriteLine("Y2: "+y);
+                y++;
+            }
+
+            //overloading (Polymorphism): 
+            // .Next() :: 0 < X < MaxValue 
+            // .Next(Int32), para1 < X < MaxValue
+            // .Next(Int32, Int32) , para1 < X < para2
+
+            //.NextDouble() Returns a random floating-point number that is greater than or equal to 0.0 and less than 1.0
+            Random rand =  new Random();
+            for (int G = 0; G < 10; G++)
+            {
+                System.Console.WriteLine(rand.Next(2,8));    
+            }
+
+        }
+    }
+}
