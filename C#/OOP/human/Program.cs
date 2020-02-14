a50a967b923a749e6f092d731d834fdd6a7521c8
tbkrft567 <tbkrft567@gmail.com>
2020-02-14 05:18:16 -0600

    Human completed!

---
+++ C#/OOP/human/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace human
+{
+class Program
+{
+    static void Main(string[] args)
+    {
+        Human zach = new Human("Zach");
+        Human shelby = new Human("Shelby");
+        Console.WriteLine(zach.Name);
+        Console.WriteLine(shelby.Name);
+        shelby.Attack(zach);
+        Console.WriteLine(zach.HealthProp);
+    }
+
+    class Human
+    {
+        // Fields for Human
+        public string Name;
+        public int Strength;
+        public int Intelligence;
+        public int Dexterity;
+        private int health;
+
+        // add a public "getter" property to access health
+        public int HealthProp
+        {
+            get{
+                return health;
+            }
+        }
+
+
+        // Add a constructor that takes a value to set Name, and set the remaining fields to default values
+        public Human(string nameParam)
+        {
+            Name = nameParam;
+            Strength = 3;
+            Intelligence = 3;
+            Dexterity = 3;
+            health = 100;
+        }
+
+        // Add a constructor to assign custom values to all fields
+        public Human(string nameParam, int strengthParam, int intelligenceParam, int dexterityParam, int healthParam){
+            Name= nameParam;
+            Strength = strengthParam; 
+            Intelligence = intelligenceParam; 
+            Dexterity = dexterityParam; 
+            health = healthParam;
+        }
+        // Build Attack method
+        public int Attack(Human target)
+        {
+            return  target.health -= (5*this.Strength);
+        }
+    }
+
+
+}
+}
